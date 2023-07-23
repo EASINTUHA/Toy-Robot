84c9bdcbec9b3981853877b9f30b383217aa068a
Easin <mdeasintuha@gmail.com>
2023-07-24 02:54:22 +0600

    first commit

--- src/hooks/useTitle.jsx
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react';
-const useTitle = title => {
-    useEffect(() => {
-        document.title = `Toy_robots | ${title}`;
-    },[title])
-}
-export default useTitle;
